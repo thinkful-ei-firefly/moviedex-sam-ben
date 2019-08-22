5cae71fce9db846fa79dc91580c71a9e373e86e3
Ben Rosen <benjamin.j.rosen@gmail.com>
2019-08-22 17:05:51 -0400

    prepare for deploy

--- src/app.js
+++ src/app.js
@@ -4,20 +4,40 @@ const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
 const store = require('./store');
+const { NODE_ENV } = require('./config');
+const logger = require('./logger');
 const app = express();
 
-app.use(morgan('dev'));
+const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
+
+app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+
+app.use(function errorHandler(error, req, res, next) {
+  let response;
+  if (NODE_ENV === 'production') {
+    response = { error: { message: 'server error' } };
+  } else {
+    console.error(error);
+    response = { message: error.message, error };
+  }
+  res.status(500).json(response);
+});
 
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN;
   const authToken = req.get('Authorization');
 
   if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    logger.error(`Unauthorized request to path: ${req.path}`);
     return res.status(401).json({ error: 'Unauthorized request' });
   }
   next();
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
 app.get('/movie', (req, res) => {
@@ -49,4 +69,4 @@ app.get('/movie', (req, res) => {
   return res.json(results);
 });
 
-app.listen(8080, () => console.log('listening on 8080'));
+module.exports = app;
